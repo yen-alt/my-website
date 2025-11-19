766fb472547f21f9c630dfaaac1ad6a7f4beb577
py <pyhuang97@gmail.com>
2025-11-19 13:54:39 -0700

    [update] repoUrl.

--- src/data/SITE.ts
+++ src/data/SITE.ts
@@ -1,8 +1,8 @@
 export const SITE = {
   name: `Showlit`,
-  repoUrl: `https://github.com/pm25/showlit`,
+  repoUrl: `https://github.com/yen-alt/my-website`,
   description: `A short description`,
   author: `Your Name`,
-  repoName: `showlit`,
-  base: `/showlit/`
+  repoName: `my-website`,
+  base: `/my-website/`
 };

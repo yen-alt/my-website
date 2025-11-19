1a8936e60d5e8b5241c7dc0aad0d732a2847b8cd
py <pyhuang97@gmail.com>
2025-11-19 12:17:53 -0700

    Updates

---
+++ assets/template-BZH86b37.js
@@ -0,0 +1,34 @@
+const n=`---
+title: "Your Post Title"
+created_at: "2025-05-20T14:30:00"
+updated_at: "2025-05-20T16:15:00"
+summary: "A one-sentence summary of the post for previews or meta description."
+tags: ["tag1", "tag2"] # optional
+cover_image: "/images/cover.jpg" # optional
+author: "Pin-Yen Huang"
+draft: true
+---
+
+# Your Post Title
+
+<!-- Optional intro section -->
+
+Welcome to my post! Here’s what we’ll cover today.
+
+## Section 1
+
+Start your content here. You can use:
+
+-   Bullet points
+-   **Bold** or _italic_ text
+-   \`inline code\`
+
+## Section 2
+
+You can also add:
+
+\`\`\`js
+// code blocks
+console.log("Hello, world!");
+\`\`\`
+`;export{n as default};
